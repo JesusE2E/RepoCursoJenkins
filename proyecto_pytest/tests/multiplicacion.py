b59900cdf8ec313fafdd20e659c643b811156760
Jesus Vazquez <jesusvb314@gmail.com>
2025-11-11 23:56:52 -0600

    proyecto py

---
+++ proyecto_pytest/tests/multiplicacion.py
@@ -0,0 +1,13 @@
+import pytest 
+
+def multiplicar(a,b):
+    """Funcion que multiplica dos numeros"""
+    return a*b
+
+def test_multiplicacion(a,b):
+    assert multiplicar(1,2)==2
+    assert multiplicar(-1,1)==-1
+    assert multiplicar(0,55)==0
+
+def test_multiplicacion_fail(a,b):
+    assert multiplicar(5,5)==500
