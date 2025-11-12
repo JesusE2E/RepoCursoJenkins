94361dbb5f5bf03e0a3c371e9c36b8d3bafe4cb2
Jesus Vazquez <jesusvb314@gmail.com>
2025-11-12 12:57:49 -0600

    Java

---
+++ java/mi-proyecto/src/main/java/com/miempresa/App.java
@@ -0,0 +1,18 @@
+package com.miempresa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int suma(int a,int b){
+        return a+b;
+
+    }
+}
---
+++ java/mi-proyecto/src/test/java/com/miempresa/AppTest.java
@@ -0,0 +1,21 @@
+package com.miempresa;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+   
+{
+   @Test
+   public void testSumar(){
+   
+assertEquals(9,App.suma(5, 4));   
+
+
+   }
+
+}
